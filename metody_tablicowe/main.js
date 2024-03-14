bdd74c1384e7a30f40c6a53f8b37ff759f1f5aac
MordarskiK <102521828+MordarskiK@users.noreply.github.com>
2024-03-14 11:09:03 +0100

    Add files via upload

--- metody_tablicowe/main.js
+++ metody_tablicowe/main.js
@@ -1,14 +1,58 @@
-const userArr = [
-    {name: 'Jasia', city: 'Gliwice', age: 16},
-    {name: 'Michał', city: 'Zabrze', age: 27},
-    {name: 'Olga', city: 'Knurów', age: 41},
-    {name: 'Marek', city: 'Bytom', age: 32},
-    {name: 'Józefa', city: 'Warszawa', age: 68},
-    {name: 'Kuba', city: 'Olsztyn', age: 12},
-    {name: 'Stasiek', city: 'Gliwice', age: 22},
-    {name: 'Kuba', city: 'Gdańsk', age: 19},
-    {name: 'Paweł', city: 'Kraków', age: 54},
-    {name: 'Zuzia', city: 'Kraków', age: 43},
+let usersArr = [
+	{
+		userID: '36b8f84d-df4e-ad32-b662-bcde71a8764f',
+		userName: 'Jasia',
+		userCity: 'Gliwice',
+		userAge: 16,	
+	},
+	{
+		userID: '36b8f84d-df4e-4d49-b662-bcde71a87623',
+		userName: 'Michał',
+		userCity: 'Zabrze',
+		userAge: 19,
+	},
+	{
+		userID: '36b8f84d-df4e-s3ae-b662-bcde71a876aa',
+		userName: 'Olga',
+		userCity: 'Knurów',
+		userAge: 23,
+	},
+	{
+		userID: '36b8f84d-df4e-a43w-b662-bcde71a876b3',
+		userName: 'Marek',
+		userCity: 'Gliwice',
+		userAge: 17,
+	},
+	{
+		userID: '36b8f84d-df4e-zfdr-b662-bcde71a876a2',
+		userName: 'Józefa',
+		userCity: 'Pyskowice',
+		userAge: 42,
+	},
+	{
+		userID: '36b8f84d-df4e-adz3-b662-bcde71a87622',
+		userName: 'Janek',
+		userCity: 'Gliwice',
+		userAge: 30,
+	},
+	{
+		userID: '36b8f84d-df4e-bfr3-b662-bcde71a87635',
+		userName: 'Hania',
+		userCity: 'Toszek',
+		userAge: 12,
+	},
+	{
+		userID: '36b8f84d-df4e-n2er-b662-bcde71a87644',
+		userName: 'Benek',
+		userCity: 'Warszawa',
+		userAge: 45,
+	},
+	{
+		userID: '36b8f84d-df4e-ke4q-b662-bcde71a876fa',
+		userName: 'Gosia',  
+		userCity: 'Gliwice',
+		userAge: 30,
+	},
 ];
 
 const nameInput = document.querySelector("#nameInput");
@@ -20,65 +64,89 @@ const userListDisplay = document.querySelector("#userListDisplay");
 addBtn.addEventListener('click', (evt)=>{
     evt.preventDefault();
     userListDisplay.innerHTML = ''
-    userListDisplay.classList.add('hidden')
+    // userListDisplay.classList.add('hidden')
 
-    userArr.forEach(element => {
+    usersArr.push({
+        userID: crypto.randomUUID(),
+		userName: nameInput.value,
+		userCity: cityInput.value,
+		userAge: ageInput.value,
+    });
+
+    usersArr.forEach(element => {
         const userListElement = document.createElement("li");
         userListElement.classList.add("list-group-item");
-        const userListDeleteBtn = 
+        userListElement.classList.add("d-flex");
+        userListElement.classList.add("justify-content-between");
+        userListElement.setAttribute("id",element.userID);  
+        // userListElement.classList.add("row");
 
-        userListElement.innerHTML = `<strong>${element.name}</strong> lat <strong>${element.age}</strong> z miasta <strong>${element.city}</strong>
-        <button type="button" class="btn btn-outline-danger p-1 position-relative">X</button>`
+        userListElement.innerHTML = `<span><strong>${element.userName}</strong> 
+                                lat <strong>${element.userAge}</strong> 
+                                z miasta <strong>${element.userCity}</strong></span>
+
+                                <button type="button" 
+                                    class="btn btn-outline-danger p-1 position-relative" 
+                                    id="${element.userID}" 
+                                    onclick="userListDeleteBtn(this.parentNode)">
+                                    X
+                                </button>`
 
         userListDisplay.appendChild(userListElement);
     });
 })
 
+function userListDeleteBtn(el){
+    var element = el;
 
-const search = document.querySelector('#search');
-const changeInput = document.querySelector('#changeInput');
+    console.log(element.id)
+
+    usersArr = usersArr.filter(elem => elem.userID!=element.id);
+
+    element.remove();
+}
+
+
+const searchText = document.querySelector('#searchText')
+const searchInput = document.querySelector('#searchInput');
 const displaySearch = document.querySelector('#displaySearch');
 const searchBtn = document.querySelector('#searchBtn');
 
-function properInputType(){
-    if(search.value=="name" || search.value=="city"){
-        changeInput.type="text";
-    }
-    else{
-        changeInput.type="number";
-    }
-}
-
-search.addEventListener('change', properInputType)
 
 searchBtn.addEventListener('click', ()=>{
     displaySearch.innerHTML = '';
-    displaySearch.classList.add('hidden')
 
-    switch (search.value){
+    switch (searchInput.value){
         case "name":
-            userArr.forEach(element => {
-                if(element.name == changeInput.value){
-                    displaySearch.innerHTML += `<li>${element.name}, ${element.city}, ${element.age} lat</li>`
+            usersArr.forEach(element => {
+                if(element.userName.toUpperCase().includes(searchText.value.toUpperCase())){
+                    displaySearch.innerHTML += `<li><strong>${element.userName}</strong> 
+                    lat <strong>${element.userAge}</strong> 
+                    z miasta <strong>${element.userCity}</strong></li>`
                 }
-            });
-            displaySearch.classList.remove('hidden');
-
+            })
+            break;
+            
         case "city":
-            userArr.forEach(element => {
-                if(element.city == changeInput.value){
-                    displaySearch.innerHTML += `<li>${element.name}, ${element.city}, ${element.age} lat</li>`
+            usersArr.forEach(element => {
+                if(element.userCity.toUpperCase().includes(searchText.value.toUpperCase())){
+                    displaySearch.innerHTML += `<li><strong>${element.userName}</strong> 
+                    lat <strong>${element.userAge}</strong> 
+                    z miasta <strong>${element.userCity}</strong></li>`
                 }
-            });
-            displaySearch.classList.remove('hidden');
-
+            })
+            break;
+            
         case "age":
-            userArr.forEach(element => {
-                if(element.age == changeInput.value){
-                    displaySearch.innerHTML += `<li>${element.name}, ${element.city}, ${element.age} lat</li>`
+            usersArr.forEach(element => {
+                if(element.userAge == searchText.value){
+                    displaySearch.innerHTML += `<li><strong>${element.userName}</strong> 
+                    lat <strong>${element.userAge}</strong> 
+                    z miasta <strong>${element.userCity}</strong></li>`
                 }
-            });
-            displaySearch.classList.remove('hidden');
+            })
+            break;
+            
     }
 })
 
